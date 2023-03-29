cc72db31d8f20064a46f11d9e9a0f75fd507b8a4
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-29 18:12:27 +0200

    add dynamic thumbnails

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,29 +4,29 @@ createApp({
     data() {
       return {
         counter: 0,
-        movie: [
+        movies: [
           {
-            image: `./assets/img/01.webp`,
+            image: './assets/img/01.webp',
             title: 'Marvel\'s Spiderman Miles Morale',
             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
           },
           {
-            image: `./assets/img/02.webp`,
+            image: './assets/img/02.webp',
             title: 'Ratchet & Clank: Rift Apart',
             text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
           }, 
           {
-            image: `./assets/img/03.webp`,
+            image: './assets/img/03.webp',
             title: 'Fortnite',
             text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
           }, 
           {
-            image: `./assets/img/04.webp`,
+            image: './assets/img/04.webp',
             title: 'Stray',
             text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
           }, 
           {
-            image: `./assets/img/05.webp`,
+            image: './assets/img/05.webp',
             title: "Marvel's Avengers",
             text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
           }
@@ -38,15 +38,19 @@ createApp({
       up() {
         this.counter--
         if(this.counter < 0) {
-          this.counter = this.movie.length - 1
+          this.counter = this.movies.length - 1
         }  
       },
 
       down() {
         this.counter++
-        if(this.counter > this.movie.length - 1) {
+        if(this.counter > this.movies.length - 1) {
           this.counter = 0
         }
+      },
+
+      currentImage(i) {
+        this.counter = i
       }
     }
   }).mount('#app')
